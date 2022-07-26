d59035e01ac5a7c4a4ddcf6ad381522dacbb8c9d
Naserpour Alireza <anaserpoor98@gmail.com>
2022-07-26 14:58:09 +0430

    Init

---
+++ src/main/java/com/example/backendinternp7/BackendInternP7Application.java
@@ -0,0 +1,13 @@
+package com.example.backendinternp7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendInternP7Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BackendInternP7Application.class, args);
+  }
+
+}
